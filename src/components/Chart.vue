72c21d0bcb9e88bcbd5a74b93a663af1db1b96b9
avorio <avorio@mail.ru>
2023-04-10 11:09:33 -0500

    Market average - virtual chart layout

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -420,7 +420,7 @@ export default {
 
       lineSeriesMarketChange = chart.addLineSeries({
         color: "gray",
-        priceScaleId: "right",
+        priceScaleId: "",
         lineWidth: this.lineWidth,
         visible: this.isLineMarketChangeVisible,
         priceLineVisible: false,
