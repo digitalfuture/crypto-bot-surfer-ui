28dfb4aa1be5387eeae82618b562c23faafdcfc3
avorio <avorio@mail.ru>
2025-07-12 21:48:38 -0500

    API - fix

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ async function readFiles() {
         // Add file name and content to the result list
         fileList.push({
           name: fileName,
-          data: fileData.trim().split("\n").slice(1).slice(2000),
+          data: fileData.trim().split("\n").slice(1).slice(-2000),
         });
       }
     }
